d370538320508b24fbc5c2d61c1d323e99306231
Denis.Torgov <deniska-3000@bk.ru>
2023-09-25 02:48:29 +0300

    Task 5 update

--- seminar_5/src/main/java/org/example/Main.java
+++ seminar_5/src/main/java/org/example/Main.java
@@ -14,16 +14,14 @@ public class Main {
 
         File[] files = file.listFiles();
         for (File f: files) {
-            if (f.getName() != null) {
-                if (f.isFile()) {
-                    Files.copy(f.toPath(), new File("../backup/" + f.toPath()).toPath(), REPLACE_EXISTING);
-                    System.out.println("File ..\\backup\\" + f.toPath().normalize() + " created");
-                } else {
-                    File dir = new File("../backup/" + f.toPath());
-                    dir.mkdirs();
-                    System.out.println("Dir  ..\\backup\\" + f.toPath().normalize() + " created");
-                    backup(f);
-                }
+            if (f.isFile()) {
+                Files.copy(f.toPath(), new File("../backup/" + f.toPath()).toPath(), REPLACE_EXISTING);
+                System.out.println("File ..\\backup\\" + f.toPath().normalize() + " created");
+            } else {
+                File dir = new File("../backup/" + f.toPath());
+                dir.mkdirs();
+                System.out.println("Dir  ..\\backup\\" + f.toPath().normalize() + " created");
+                backup(f);
             }
         }
     }
